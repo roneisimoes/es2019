fccdb2523e0944c614cc7079cee35c5408dc8cf5
unknown <IFSC@CDR-739665.cacador.ifsc.edu.br>
2019-10-09 21:01:22 -0300

    Adcionado DAOProspect

--- models/Prospect.php
+++ models/Prospect.php
@@ -1,121 +1,100 @@
 <?php
-    namespace models;
-
-    class Prospect{
-        public $idprospect;
-        public $nome;
-        public $email;
-        public $celular;
-        public $facebook;
-        public $whatsapp;
-        public $rua;
-        public $numero;
-        public $bairro;
-        public $cidade;
-        public $uf;
-        public $cep;
-    
-    public function incluirProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep){
-        $conexaoDB = $this-> conectarbanco();
-        
-        $sqlInsert = $conexaoDB->prepare("insert into prospect
-                                        (nome, email, celular, facebook, whatsapp, rua, numero, bairro, cidade, uf, cep)
-                                        values
-                                        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $sqlInsert ->bind_param("ssssssissss", $nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep);
-        
-        $sqlInsert-> execute();
-        if(!$sqlInsert->error){
-           $retorno = TRUE;
-        }else{
-           $retorno = FALSE;
-        }
-        $conexaoDB->close();
-        $sqlInsert->close();
-    
-        return $retorno;
-    }
-
-   
-
-    public function atualizarProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep, $idprospect){
-        $conexaoDB = $this-> conectarbanco();
-        
-        $sqlUpdate = $conexaoDB->prepare("UPDATE prospect
-                                        SET nome=? , email=?, celular=?, facebook=?, whatsapp=?, rua=?, numero=?, bairro=?, cidade=?, uf=?, cep=?
-                                        WHERE idprospect =?");
-        $sqlUpdate ->bind_param("ssssssissssi", $nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep, $idprospect);
-        
-        $sqlUpdate-> execute();
-        if(!$sqlUpdate->error){
-            $retorno = TRUE;
-         }else{
-            $retorno = FALSE;
-         }
-         $conexaoDB->close();
-         $sqlUpdate->close();
-         return $retorno;
-    }
-
-    public function excluirProspect($idprospect){
-        $conexaoDB = $this-> conectarbanco();
-        
-        $sqlDelete = $conexaoDB->prepare("DELETE FROM prospect WHERE idprospect =?");
-        $sqlDelete ->bind_param("i", $idprospect);
-        
-        $sqlDelete-> execute();
-        if(!$sqlDelete->error){
-            $retorno = TRUE;
-         }else{
-            $retorno = FALSE;
-         }
-         return $retorno;
-
-    }
-
-    public function buscarProspect($email=null){
-        $conexaoDB = $this-> conectarbanco();
-        $prospects = array();
-        
-        if($email == null){
-                    
-            $sqlBusca = $conexaoDB->prepare("select idprospect, nome, email, celular, facebook, whatsapp, rua, numero, bairro, cidade, uf, cep from prospect");
-            $sqlBusca->execute();
-        }else{
-            $sqlBusca = $conexaoDB->prepare("select idprospect, nome, email, celular, facebook, whatsapp, rua, numero, bairro, cidade, uf, cep from prospect where email = ?");
-            $sqlBusca->bind_param("s", $email);
-            $sqlBusca->execute();
-        }
-
-        $resultado = $sqlBusca->get_result();
-        if($resultado->num_rows !== 0){
-            while($linha = $resultado->fetch_assoc()){
-                $prospect = array(
-                    "idprospect" => $linha['idprospect'],
-                    "nome" => $linha['nome'],
-                    "email" => $linha['email'],
-                    "celular" => $linha['celular'],
-                    "facebook" => $linha['facebook'],
-                    "whatsapp" => $linha['whatsapp'],
-                    "rua" => $linha['rua'],
-                    "numero" => $linha['numero'],
-                    "bairro" => $linha['bairro'],
-                    "cidade" => $linha['cidade'],
-                    "uf" => $linha['uf'],
-                    "cep" => $linha['cep']
-                );
-                $prospects[] = $prospect;
-            }
-        }
-        
-        $conexaoDB->close();
-        $sqlBusca->close();       
-        return $prospects;
-    }
-
-    private function conectarBanco(){
-        $conn = new \mysqli('localhost', 'root', '', 'mydb');
-        return $conn;
-    }
-    }
+namespace MODELS;
+/**
+ * Classe Model de usuário
+ * @author Ronei/Vitor
+ * @package MODELS
+ */
+class Prospect{
+/**
+ * Identifica Prospect
+ * @var int
+ */
+   public $idprospect;
+   /**
+ * Nome do Prospect
+ * @var string
+ */
+   public $nome;
+   /**
+ * EMAIL do Prospect
+ * @var string
+ */
+   public $email;
+   /**
+ * Celular do Prospect
+ * @var string
+ */
+   public $celular;
+   /**
+ * Facebookndo Prospect
+ * @var string
+ */
+   public $facebook;
+   /**
+ * Whatsapp do Prospect
+ * @var string
+ */
+   public $whatsapp;
+   /**
+ * Rua do endereco do Prospect
+ * @var string
+ */
+   public $rua;
+   /**
+ * Numero do endereco do Prospect
+ * @var int
+ */
+   public $numero;
+/**
+ * Bairro do endereco do Prospect
+ * @var string
+ */
+   public $bairro;
+/**
+ * Cidade do Prospect
+ * @var string
+ */
+   public $cidade;
+/**
+ * Uf do Prospect
+ * @var string
+ */
+   public $uf;
+/**
+ * Cep do Prospect
+ * @var string
+ */
+   public $cep;
+/**
+ * Carrega os atributos da classe
+ * @param int $idprospect Identificador do Prospect
+ * @param string $nome Nome do Prospect
+ * @param string $email Email do Prospect
+ * @param string $celular Celular do Prospect
+ * @param string $facebook Facebook do Prospect
+ * @param string $whatsapp Whatsapp do Prospect
+ * @param string $rua Endereco do Prospect
+ * @param string $numero Numero do endereco do Prospect
+ * @param string $bairro Bairro do Prospect
+ * @param string $cidade Cidade do Prospect
+ * @param string $uf UF do Prospect
+ * @param string $cep CEP do Prospect
+ * @return void
+ */
+   public function addProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep){
+        $this->idprospect = $idprospect;
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->celular = $celular;
+        $this->facebook = $facebook;
+        $this->whatsapp = $whatsapp;
+        $this->rua = $rua;
+        $this->numero = $numero;
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+        $this->uf = $uf;
+        $this->cep = $cep;
+   }
+}
 ?>
