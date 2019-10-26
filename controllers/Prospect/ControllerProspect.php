302e7d81485fbcc0bfe3b6340edcaf8d20abd03b
unknown <IFSC@CDR-739665.cacador.ifsc.edu.br>
2019-10-25 21:05:45 -0300

    Atualizado

--- controllers/Prospect/ControllerProspect.php
+++ controllers/Prospect/ControllerProspect.php
@@ -1,47 +1,74 @@
 <?php
 namespace CONTROLLERS;
-require_once('../../DAO/DAOUsuario.php');
-use DAO\DAOUsuario;
+require_once('../../DAO/DAOProspect.php');
+use DAO\DAOProspect;
 
 /**
  * Esta classe é responsavel por fazer o tratamento dos dados para a apresentaçao e/ou
  * envio para o DAO
- * Seu espaco limita se as funcoes da entidade Usuario
+ * Seu espaco limita se as funcoes da entidade Prospect
  * 
  * @author Ronei/Vitor
  */
 class ControllerUsuario{
     /**
-     * Recebe os dados de login, faz o devido tratamento e envia para o DAO executar 
+     * Recebe os dados, faz o devido tratamento e envia para o DAO executar 
      * no banco de dados
      * @param string $login Login do usuario
      * @param string $senha Senha do usuario
      * @return Usuario
      */
-    public function fazerLogin($login, $senha){
-        $daoUsuario = new DAOUsuario();
+    public function incluirProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep){
+        $daoProspect = new DAOProspect();
 
-        $usuario= $daoUsuario->logar($login, $senha);
+        $Prospect= $daoProspect->incluir($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep);
 
-        unset($daoUsuario);
-        return $usuario;
+        unset($daoProspect);
+        return $prospect;
     }
-    public function salvarUsuario($nome, $email, $login, $senha){
-        $daoUsuario = new DAOUsuario();
+
+    public function atualizarProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep, $idprospect){
+        $daoProspect = new DAOProspect();
         /**
          * Captura a excecao retornada pela DAO no caso de falha ao incluir um usuario
          * e dispara outra excecao para ser tratada por quem chamar esta funcao
          */
         try{
-           $retorno = $daoUsuario->incluirUsuario($nome, $email, $login, $senha);
-           unset($daoUsuario);
+           $retorno = $daoProspect->atualizarProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep, $idprospect);
+           unset($daoProspect);
            return $retorno; 
         }catch(\Exception $e){
             throw new \Exception($e->getMessage());
         }
     }
 
+    public function excluirProspect($idprospect){
+        $daoProspect = new DAOProspect();
+        
+        try{
+            $retorno = $daoProspect->prepare("DELETE FROM prospect WHERE idprospect =?");
+            $sqlDelete ->bind_param("i", $idprospect);
+            unset($daoProspect);
+            return $retorno; 
+         }catch(\Exception $e){
+             throw new \Exception($e->getMessage());
+         }
 
+    }
+
+
+    public function buscarProspect($email=null){
+        $daoProspect = new DAOProspect();
+        
+        try{
+            $retorno = $daoProspect->buscarProspect($nome, $email, $celular, $facebook, $whatsapp, $rua, $numero, $bairro, $cidade, $uf, $cep, $idprospect);
+            unset($daoProspect);
+            return $retorno; 
+         }catch(\Exception $e){
+             throw new \Exception($e->getMessage());
+         }
+        }
+        
 }
 
 
